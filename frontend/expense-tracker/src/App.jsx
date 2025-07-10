d0710840001a482d7dbcc77a86d458178c0d0dc2
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-07-10 10:19:19 +0530

    implemented lazy loading

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{lazy,Suspense} from 'react'
 import './index.css'
 import {
   BrowserRouter as Router,
@@ -8,9 +8,12 @@ import {
 } from 'react-router-dom'
 import Login from './pages/Auth/Login'
 import SignUp from './pages/Auth/SignUp'
-import Home from './pages/Dashboard/Home'
-import Income from './pages/Dashboard/Income'
-import Expense from './pages/Dashboard/Expense'
+//import Home from './pages/Dashboard/Home'
+const Home = lazy(()=>import('./pages/Dashboard/Home'))
+//import Income from './pages/Dashboard/Income'
+const Income = lazy(()=>import('./pages/Dashboard/Income'))
+//import Expense from './pages/Dashboard/Expense'
+const Expense = lazy(()=>import('./pages/Dashboard/Expense'))
 import {UserProvider} from './context/UserContext'
 import {Toaster} from 'react-hot-toast'
 
@@ -21,11 +24,14 @@ const App = () => {
      <Router>
         <Routes>
           <Route path='/' element={<Root />} />
-          <Route path='/login' exact element={<Login />} />
-          <Route path='/signUp' exact element={<SignUp />} />
-          <Route path='/dashboard' exact element={<Home />} />
-          <Route path='/income' exact element={<Income />} />
-          <Route path='/expense' exact element={<Expense />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signUp' element={<SignUp />} />
+          {/* <Route path='/dashboard' element={<Home />} /> */}
+          <Route path='/dashboard' element={<Suspense fallback={<div>Loading Dashboard...</div>}><Home /></Suspense>} />
+          {/* <Route path='/income' element={<Income />} /> */}
+          <Route path='/income' element={<Suspense fallback={<div>Loading Income...</div>}><Income /></Suspense>} />
+          {/* <Route path='/expense' element={<Expense />} /> */}
+          <Route path='/expense' element={<Suspense fallback={<div>Loading Expense...</div>}><Expense /></Suspense>} />
         </Routes>
       </Router>
     </div>
